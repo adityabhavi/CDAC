2fd85cc0ebb0d5ffc5aadf11e2c49e2bb65f62cc
adityabhavi <65148702+adityabhavi@users.noreply.github.com>
2020-10-07 00:10:58 +0530

    Q67

---
+++ Assignment 63-67/Q67.java
@@ -0,0 +1,39 @@
+  
+//67. Write a program to read data from shopping.dat file creted in above 
+// problem and find total money spent on all shopping items. . 
+//(Use ObjectInputStream to read Item class object).
+ 
+
+package Q67;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+public class Q67 {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+		FileInputStream f = new FileInputStream("H:\\Study\\java\\prac\\file1.txt");
+		ObjectInputStream o = new ObjectInputStream(f);
+		
+		ArrayList<Item> a = new ArrayList<>();
+		Object obj = null;
+	
+boolean flag = true;
+		while(flag) {
+			if(f.available() != 0) {
+				obj = (Item)o.readObject();
+				a.add((Item) obj);
+			}else {
+				flag = false;
+			}
+	    }
+		for(Item x : a) {
+			System.out.println(x);
+		}
+
+		o.close();
+}
+}
