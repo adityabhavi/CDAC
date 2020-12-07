96969f0a793f50889da0d8103561a06ced43166d
Aditya Bhavi <65148702+adityabhavi@users.noreply.github.com>
2020-12-07 17:04:51 +0530

    Assignment-2.1

--- Assignment-2/Program.cs
+++ Assignment-2/Program.cs
@@ -4,101 +4,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InheritanceExample
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Employee e1 = new Manager("abc", 3, 2000, "tech-manager");
-            Employee e2 = new GeneralManager("xyz", 3,20000 ,"bhaiya ji", "CEO");
-            Employee e3 = new CEO("Aditya", 5, 1000);
-            Console.WriteLine(e3.CalcNetSalary());
-            Console.WriteLine(e2.CalcNetSalary());
+            Employee o1 = new Employee("Amol", 12346, 10);
+            Employee o2 = new Employee("Amol", 12346);
+            Employee o3 = new Employee("Amol");
+            Employee o4 = new Employee();
 
-            Console.WriteLine(e1.CalcNetSalary());
-            Console.WriteLine(e1.EmpNo);
-            Console.WriteLine(e2.EmpNo);
-            Console.WriteLine(e3.EmpNo);
+            Console.WriteLine(o1.EMPNO);
+            Console.WriteLine(o2.EMPNO);
+            Console.WriteLine(o3.EMPNO);
+            
+            Console.WriteLine(o3.EMPNO);
+            Console.WriteLine(o2.EMPNO);
+            Console.WriteLine(o1.EMPNO);
             Console.ReadLine();
         }
     }
-
-    public abstract class Employee
-    {
-
-        public Employee(string name = null, decimal basic = 0 , short deptNo =0 )
+    class Employee
+    {   
+        public Employee(string name=null, decimal basic=0, short deptNo=0)
         {
-            empNo = ++eno;
-            this.Name = name;
-            this.Basic = basic;
-            this.DeptNo = deptNo;
-                
+            //empNo=++empNo;
+            empNo = ++eNo;
+            NAME = name;
+            BASIC = basic;
+            DEPTNO = deptNo;
         }
 
-
-        public abstract decimal CalcNetSalary();
-
+     
+        #region Properties
         private string name;
-        public string Name
+        public string NAME
         {
             set
             {
-                if(value != null)
+                if (value != null)
                 {
                     name = value;
                 }
                 else
-                {
-                    Console.WriteLine("Invalid Name Emtry");
-                }
+                   Console.WriteLine("Invalid Entry name");
             }
             get
             {
                 return name;
             }
         }
-        protected decimal basic;
 
-        public abstract decimal Basic
-        {
-            set;
-            get;
-        }
-
-        private static int eno;
-        public static int ENO
+        private decimal basic;
+        public decimal BASIC
         {
             set
             {
-                eno = 0;
+                if(value < 150000 && value > 0)
+                {
+                    basic = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Basic");
+                }
             }
             get
             {
-                return eno;
-            }
-        }
-
-        private int empNo;
-        public int EmpNo
-        {
-            get
-            {
-                return empNo;
+                return basic;
             }
         }
 
         private short deptNo;
-        public short DeptNo
+        public short DEPTNO
         {
             set
             {
-                if(value <= 0)
+                if (value <= 0)
                 {
-                    Console.WriteLine("Invalid Dept No");
+                    Console.WriteLine("Invalid Dept No !!");
                 }
                 else
+                {
                     deptNo = value;
+                }
             }
             get
             {
@@ -106,104 +96,36 @@ namespace InheritanceExample
             }
         }
 
-    }
+        private int empNo;
 
-    class Manager : Employee
-    {
-        public Manager(string name = null, decimal basic = 0, short deptNo = 0, string designation = null):base(name,basic,deptNo)
-        {
-            this.Designation = designation;
-        }
-        public override decimal Basic
-        {
-            set
-            {
-                basic = value;
-            }
-            get
-            {
-                return Basic;
-            }
-        }
-
-        private string designation;
-        public string Designation
-        {
-            set
-            {
-                if(value != null)
-                {
-                    designation = value;
-                }
-                else
-                {
-                    Console.WriteLine("Desination can't be blank");
-                }
-            }
-            get
-            {
-                return designation;
-            }
-        }
-
-        public override decimal CalcNetSalary()
-        {
-            Console.WriteLine("This is Manager net salary method");
-            return 10000;
-        }
-    }
-
-    class GeneralManager : Manager
-    {
-        public GeneralManager(string name = null, decimal basic = 0, short deptNo = 0, string designation = null, string perks = null):base(name,basic,deptNo,designation)
-        {
-                
-        }
-        private string perks;
-        public string Perks
-        {
-            set
-            {
-                perks = value;
-            }
-            get
-            {
-                return perks;
-            }
-        }
-
-        public override decimal CalcNetSalary()
-        {
-            Console.WriteLine("This is General Manager net salary method");
-            return 10000;
-        }
-
-
-    }
-
-    class CEO : Employee
-    {
-        public CEO(string name = null, decimal basic = 0, short deptNo = 0):base(name,basic,deptNo)
-        {
-                
-        }
-        public override decimal Basic
+        public int EMPNO
         {
             get
             {
-                return basic;
+                return empNo;
             }
+        }
+
+        private static int eNo;
+        public static int ENO
+        {
             set
             {
-                basic = value;
+                eNo = 0;
+            }
+            get
+            {
+                return eNo;
             }
         }
-        public override sealed decimal CalcNetSalary()
-        {
-            Console.WriteLine("This is CEO salary method");
-            return 1010101010;
-        }
-    }
+        #endregion
+       
 
+        public decimal getNetSalary()
+        {
+            return BASIC + 2000 + 3000;
+        }
+
+    }
 
 }
